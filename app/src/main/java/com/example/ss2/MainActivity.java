4ea21fcb7688bfc3b27e8403e86e18e0280d895f
your name <nguyenminhduc01032001@gmail.com>
2023-02-13 21:22:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ss2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.ss2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.btap);
+        setContentView(R.layout.baitap2);
+        setContentView(R.layout.taitap3);
+
+    }
+}
